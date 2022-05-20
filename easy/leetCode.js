cdd0f028618dd5449c8dcf7478c824ec2f23ea07
Simone Odegard <simone.odegard@gmail.com>
2022-05-20 14:33:28 -0700

    attempted is palindrome

--- easy/leetCode.js
+++ easy/leetCode.js
@@ -20,22 +20,22 @@
 let nums = [2, 7, 11, 15];
 let target = 9;
 
-function twoSum (nums, target) {
+function twoSum(nums, target) {
   let result = [];
   for (let i = 0; i < nums.length; i++) {
-      for (let j = i+1; j < nums.length; j++) {
-          if (nums[j] == target - nums[i]) {
-              findIndex1 = nums.findIndex((index) => index == nums[i]);
-              result.push(findIndex1);
-              findIndex2 = nums.findIndex((index) => index == nums[j]);
-              result.push(findIndex2);
-              return result;
-          }
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] == target - nums[i]) {
+        findIndex1 = nums.findIndex((index) => index == nums[i]);
+        result.push(findIndex1);
+        findIndex2 = nums.findIndex((index) => index == nums[j]);
+        result.push(findIndex2);
+        return result;
       }
+    }
   }
 }
 
-console.log(twoSum(nums, target));
+console.log('two sum', twoSum(nums, target));
 
 // ======================================================= //
 
@@ -50,14 +50,72 @@ console.log(twoSum(nums, target));
 
 // INCOMPETE!!!!!
 
-var removeDuplicates = function(nums) {
+let arr = [1, 1, 2]
+
+var removeDuplicates = function (arr) {
+  // create new empty array to put numbers that are not duplicates into
   let result = [];
-  for (let i = 0; i < nums; i++) {
-      if (nums[i] != nums[i]-1) {
-          result.push(nums[i]);
-          return result;
-      }
+
+  // loop through the array
+  for (let i = 0; i < arr; i++) {
+
+    // if statement. if the number at x index is not equal to the number at x index - 1 then push that number into the new array. return the new array
+    if (arr[i] != arr[i - 1]) {
+      result.push(arr[i]);
+      return result;
+    }
   }
 };
 
-console.log(removeDuplicates(nums));
+console.log('remove duplicates', removeDuplicates(arr));
+
+// ======================================================= //
+
+// **  C H A L L E N G E   0 0 3  ** //
+// **  I S   P A L I N D R O M E   ?  ** //
+
+// Determine if input is a palindrome
+
+// Example 1:
+// Input: x = 121
+// Output: true
+
+// Example 2:
+// Input: x = -121
+// Output: false
+
+// !!! current code works on strings and palindrome numbers. does not work on non-palindrome numbers
+
+// let x = 121
+// let x = 'dad'
+let x = 123
+
+var isPalindrome = function(x) {
+  // make it easier to read
+  let int = x
+  
+  // negative numbers won't work
+  if (int < 0) {
+      return false;
+  }
+  
+  // less than 10 will work because single digit
+  if (int < 10) {
+      return true;
+  }
+  
+  // loop through half of integer
+  for (let i = 0; i < int.length / 2; i++) {
+
+      // check if first and last parts are same
+      if (int[i] !== int[int.length - 1 - i]) {
+          return false;
+      }
+  }
+  return true;
+};
+
+// variable to call the function
+let palindrome = isPalindrome(x);
+
+console.log('is palindrome', palindrome);
