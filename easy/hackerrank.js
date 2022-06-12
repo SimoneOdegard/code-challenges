ed9cef2fc43a5ab0352e0e3f028dfcff07ad6ca1
Simone Odegard <simone.odegard@gmail.com>
2022-06-12 08:46:39 -0700

    hackerrank updated

--- easy/hackerrank.js
+++ easy/hackerrank.js
@@ -86,3 +86,68 @@ function jumpingOnClouds(c) {
 }
 
 console.log('jumping on clouds--', jumpingOnClouds(c));
+
+// ======================================================= //
+
+// **  C H A L L E N G E   0 0 3  ** //
+// **  C O U N T   V A L L E Y S  ** //
+
+// An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly STEPS steps, for every step it was noted if it was an uphill, U, or a downhill, D step. Hikes always start and end at sea level, and each step up or down represents a 1 unit change in altitude. We define the following terms:
+
+// A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+// A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+// Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+// Example:
+// steps = 8 path = [DDUUUUDD]
+// The hiker first enters a valley 2 units deep. Then they climb out and up onto a mountain 2 units high. Finally, the hiker returns to sea level and ends the hike.
+
+// int steps = number of steps on the hike
+// string path = a string describing the path
+// returns the number of valleys traversed
+
+let steps = 8;
+let valleyStr = 'UDDDUDUU';
+// output should be 1
+let valleyStr2 = 'DDUUDDUU';
+// output should be 2
+
+function countValleys(steps, str) {
+  let up = 0;
+  let down = 0;
+  let sea = 0;
+  let valley = 0; 
+  for (let i = 0; i < steps; i++) {
+    if (str[i] === 'U') {
+      up++;
+      sea = sea + up;
+      if (sea === 0) {
+        valley++;
+      }
+    }
+    if (str[i] === 'D') {
+      down++;
+      sea = sea - down;
+    }
+  }
+  return valley;
+}
+
+function countValleysRefact(steps, str) {
+  let sea = 0;
+  let valley = 0;
+  for (let i = 0; i < steps; i++) {
+    if (str[i] === 'U') {
+      sea++
+      if (sea === 0) {
+        valley++;
+      }
+    }
+    if (str[i] === 'D') {
+      sea--;
+    }
+  }
+  return valley;
+}
+
+console.log('counting valleys--', countValleysRefact(steps, valleyStr));
