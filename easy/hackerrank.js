24b83851e1d586a1b1e4317b49134ff782805740
Simone Odegard <simone.odegard@gmail.com>
2022-06-13 10:43:54 -0700

    completed sales by match

--- easy/hackerrank.js
+++ easy/hackerrank.js
@@ -151,3 +151,33 @@ function countValleysRefact(steps, str) {
 }
 
 console.log('counting valleys--', countValleysRefact(steps, valleyStr));
+
+// ======================================================= //
+
+// **  C H A L L E N G E   0 0 4  ** //
+// **  S A L E S   B Y   M A T C H  ** //
+
+// There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+// Example
+// n = 7
+// arr = [1, 2, 1, 2, 1, 3, 2];
+
+// There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
+
+let socks = [1, 2, 1, 2, 1, 3, 2];
+let socks2 = [1, 2, 1, 2, 1, 2, 1, 3];
+
+function sockMatch(arr, n) {
+  let count = 0;
+  let sorted = arr.sort();
+  for (let i = 0; i < n; i++) {
+    if (sorted[i] === sorted[i+1]) {
+      count++;
+      i++;
+    }
+  }
+  return count;
+};
+
+console.log('sock match--', sockMatch(socks2, 7));
