3e803f33bc5335eb24a803b6a893eeb04fecf796
Simone Odegard <simone.odegard@gmail.com>
2022-06-14 11:43:13 -0700

    completed left rotation

--- easy/hackerrank.js
+++ easy/hackerrank.js
@@ -181,3 +181,27 @@ function sockMatch(arr, n) {
 };
 
 console.log('sock match--', sockMatch(socks2, 7));
+
+// ======================================================= //
+
+// **  C H A L L E N G E   0 0 5  ** //
+// **  L E F T   R O T A T I O N  ** //
+
+// A left rotation operation on an array shifts each of the array's elements 1 unit to the left. For example, if 2 left rotations are performed on array [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2]. Note that the lowest index item moves to the highest index in a rotation. This is called a circular array.
+
+// Given an array a of n integers and a number, d, perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+let rotation = [1, 2, 3, 4, 5];
+
+function leftRotation(arr, d) {
+  let spliced = arr.splice(d);
+  console.log('spliced arr--', spliced);
+  for (let i = 0; i < arr.length; i++) {
+    if (i < d) {
+      spliced.push(arr[i]);
+    }
+  }
+  return spliced;
+};
+
+console.log('left rotation--', leftRotation(rotation, 2));
