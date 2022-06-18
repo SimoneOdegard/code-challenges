d7fe88902152e46bb85d2d66a4af3e69158f372a
Simone Odegard <simone.odegard@gmail.com>
2022-06-18 11:33:14 -0700

    completed alternating characters

--- easy/hackerrank.js
+++ easy/hackerrank.js
@@ -332,4 +332,35 @@ function makeAnagram(a, b) {
   return answer;
 };
 
-  console.log('anagram--', makeAnagram(stringA2, stringB2));
+console.log('anagram--', makeAnagram(stringA2, stringB2));
+
+// ======================================================= //
+
+// **  C H A L L E N G E   0 0 9  ** //
+// **  A L T E R N A T I N G  ** //
+
+// You are given a string containing characters A and B only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+
+// Your task is to find the minimum number of required deletions.
+
+// Example
+// s = AABAAB
+// Remove an A at positions 0 and 3 to make s = ABAB in 2 deletions
+
+let altStr1 = 'AAAA'; // 3
+let altStr2 = 'BBBBB'; // 4
+let altStr3 = 'ABAB'; // 0
+let altStr4 = 'AAABBB'; // 4
+let altStr5 = 'AABBABABB'; // 3
+
+function alternatingCharacters(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log('alternating characters--', alternatingCharacters(altStr2));
