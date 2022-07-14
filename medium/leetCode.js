d454bf3e0162098791b9fdb6a1419766c0d48f20
Simone Odegard <simone.odegard@gmail.com>
2022-07-13 18:57:22 -0700

    set up challenges for the next few days at camp

--- medium/leetCode.js
+++ medium/leetCode.js
@@ -1,6 +1,6 @@
 // ======================================================= //
 
-// **  C H A L L E N G E   0 0 1  ** //
+// **  C H A L L E N G E   0 1 3  ** //
 // **  S I N G L E   N U M B E R   2  ** //
 
 // Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
